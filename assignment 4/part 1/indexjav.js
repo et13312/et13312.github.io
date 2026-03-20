d300a7603bcee8620ea9d52c378b7e61cb89c7a6
et13312 <etfuerth@gmail.com>
2026-03-20 17:11:50 -0400

    Update indexjav.js

--- assignment 4/part 1/indexjav.js
+++ assignment 4/part 1/indexjav.js
@@ -8,22 +8,13 @@ function randomValueFromArray(array) {
   const random = Math.floor(Math.random() * array.length);
   return array[random];
 }
-
-// Raw text strings
-
-// Willy the Goblin
-// Big Daddy
-// Father Christmas
-
-// the soup kitchen
-// Disneyland
-// the White House
-
-// spontaneously combusted
-// melted into a puddle on the sidewalk
-// turned into a slug and slithered away
-
-// Partial return random string function
+storyText ("Willy the Goblin",
+    "Big Daddy,Father Christmas", 
+    "the soup kitchen", 
+    "Disneyland", 
+    "the White House", 
+    "spontaneously combusted", 
+    "melted into a puddle on the sidewalk")
 
 function returnRandomStoryString() {
   // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.

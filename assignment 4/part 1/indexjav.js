8cc8cd5236501095ee294e29954f4caa459c3dd6
et13312 <etfuerth@gmail.com>
2026-04-03 18:24:10 -0400

    Error fixes
    Working story

--- assignment 4/part 1/indexjav.js
+++ assignment 4/part 1/indexjav.js
@@ -16,6 +16,7 @@ const places = ["The Soup Kitchen", "Disneyland", "The White House"]
 const events= ["spontaneously combusted", "melted into a puddle on the sidewalk"]
 
 function returnRandomStoryString() {
+  let  newStory = storyText;
 const randomCharacter = randomValueFromArray(characters);
 const randomPlace = randomValueFromArray(places);
 const randomEvent = randomValueFromArray(events);
@@ -24,29 +25,31 @@ newStory = newStory
  .replaceAll (":randomCharacter:", randomCharacter)
  .replace(":randomPlace:", randomPlace)
  .replace(":randomEvent:", randomEvent)
+
+ return newStory;
 }
 // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
 // Event listener and partial generate function definition
 
-let newStory = returnRandomStoryString
 
 generateBtn.addEventListener("click", generateStory);
 
 function generateStory() {
+  let newStory = returnRandomStoryString();
+
   if (customName.value !== "") {
     const name = customName.value;
-  newStory = newStory.replace("Bob",name);
+    newStory = newStory.replace("Bob",name);
   }
 
   if (document.getElementById("uk").checked) {
-    const weight = Math.round(300/14); + 'stone'
+    const weight = Math.round(300/14) + ' stone'
     const temperature = Math.round(94 - 30 / 2) + 'Celsius';
-    newStory.replace("300 pounds", weight);
-    newStory.replace ("94 Fahrenheit", temperature);
+    newStory = newStory.replace("300 pounds", weight);
+    newStory = newStory.replace ("94 Fahrenheit", temperature);
   }
 
   // TODO: replace "" with the correct expression
-  story.textContent = "";
   story.textContent = newStory
   story.style.visibility = "visible";
 }

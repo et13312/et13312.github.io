779fb08c4baa415ea46ebfd67a1e2b460252f03e
et13312 <etfuerth@gmail.com>
2026-04-12 13:11:10 -0400

    EvilCircle constructor

--- assignment 4/part 4/main.js
+++ assignment 4/part 4/main.js
@@ -36,6 +36,30 @@ class Ball extends Shape {
     this.color = color;
     this.size = size;
   }
+}
+class EvilCircle extends Shape {
+  constructor(x, y) {
+  super(x, y, 20, 20)
+  this.color = "white";
+  this.size = 10;
+
+  window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "a":
+      this.x -= this.velX;
+      break;
+    case "d":
+      this.x += this.velX;
+      break;
+    case "w":
+      this.y -= this.velY;
+      break;
+    case "s":
+      this.y += this.velY;
+      break;
+  }
+});
+}
 
   draw() {
     ctx.beginPath();
